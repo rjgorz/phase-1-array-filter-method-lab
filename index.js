a5966360512515ef5ca2fda2941ed01510456011
Robert Gorz <rjgorz@gmail.com>
2023-01-10 18:16:59 -0600

    completed functions using .filter()

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(arr, search) {
+    return arr.filter(
+      (match) => match.toLowerCase() === search.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(arr, string) {
+    return arr.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(arr, nameSearch) {
+    return arr.filter((record) => record.name === nameSearch);
+  }
+  
